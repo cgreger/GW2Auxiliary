2b63070204d75b764a85bab549fafe69d73d2d88
cgreger <greger@madisoncollege.edu>
2017-03-01 21:35:15 -0600

    created passing test for setItemRecipes itemdao function

--- src/test/java/com/cgreger/persistence/ItemDAOTest.java
+++ src/test/java/com/cgreger/persistence/ItemDAOTest.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
@@ -39,14 +40,30 @@ public class ItemDAOTest {
     @Test
     public void getItemRecipes() throws Exception {
 
-        ItemDAO dao = new ItemDAO();
         ArrayList<Integer> recipes = dao.getItemRecipes(50065);
         ArrayList<Integer> recipesExpected = new ArrayList<Integer>();
         recipesExpected.add(8455);
         recipesExpected.add(8459);
         recipesExpected.add(8460);
 
-        assertEquals("Failed to retrieve list of Item Recipes.", recipesExpected, recipes);
+        assertEquals("Failed to retrieve list of Item Recipes for Item (id50065)", recipesExpected, recipes);
+
+    }
+
+    @Test
+    public void setItemRecipes() throws Exception {
+
+        Item item = new Item();
+
+        ArrayList<Integer> recipes = new ArrayList<Integer>();
+        recipes.add(8455);
+        recipes.add(8459);
+        recipes.add(8460);
+
+        item.setRecipes(recipes);
+
+        assertEquals("Failed to set Recipes for Item (id" + item.getId() + ")",
+                recipes, item.getRecipes());
 
     }
 

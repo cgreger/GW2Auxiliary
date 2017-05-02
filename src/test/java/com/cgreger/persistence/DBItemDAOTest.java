bd9beffcb371ed6b0cfc69b0a47d1522062e93bc
cgreger <greger@madisoncollege.edu>
2017-05-02 14:00:17 -0500

    can query items with lucene

--- src/test/java/com/cgreger/persistence/DBItemDAOTest.java
+++ src/test/java/com/cgreger/persistence/DBItemDAOTest.java
@@ -1,6 +1,18 @@
 package com.cgreger.persistence;
 
 import com.cgreger.entity.db.DBItem;
+import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,12 +21,17 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class DBItemDAOTest {
 
     private DBItemDAO dbItemDAO = new DBItemDAO();
     private static SessionFactory factory = SessionFactoryProvider.getSessionFactory();
+    private Logger log = Logger.getLogger(this.getClass());
 
     @BeforeClass
     public static void runOnceBeforeClass() throws Exception {
@@ -28,7 +45,7 @@ public class DBItemDAOTest {
 
             session.createSQLQuery("INSERT INTO TEST_gw2_auxiliary.item " +
                     "(gw2_id, name, type) VALUES" +
-                    "(1, 'MONSTER ONLY Moa Unarmed Pet', 'Weapon')," +
+                    //"(1, 'MONSTER ONLY Moa Unarmed Pet', 'Weapon')," +
                     "(2, 'Assassin Pill', 'Consumable')," +
                     "(6, '((208738))', 'Weapon')," +
                     "(11, 'Undead Unarmed', 'Weapon')," +
@@ -108,6 +125,54 @@ public class DBItemDAOTest {
         dbItemDAO.truncateItemDatabase();
 
         //TODO: Check that table is empty
+
+    }
+
+    @Test
+    public void indexingTest() throws Exception {
+        Session session = factory.openSession();
+        List<DBItem> dbItems = session.createCriteria(DBItem.class).list();
+        Analyzer analyzer = new StandardAnalyzer();
+
+        // Store the index in memory:
+        //Directory directory = new RAMDirectory();
+        // To store an index on disk, use this instead:
+        Directory directory = FSDirectory.open(Paths.get("/home/katana/EnterpriseRepos/GW2Auxiliary/lucene/indexes/testindex"));
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter iwriter = new IndexWriter(directory, config);
+
+        // TODO: Must be in this order!!
+        for (DBItem dbItem : dbItems) {
+            Document doc = new Document();
+            doc.add(new Field("item_name", dbItem.getName(), TextField.TYPE_STORED));
+            doc.add(new Field("item_type", dbItem.getType(), TextField.TYPE_STORED));
+            log.info(dbItem.getGw2Id());
+            iwriter.addDocument(doc);
+        }
+
+
+        iwriter.close();
+
+        String querystr = "wepon";
+        Query fuzzyQuery = new FuzzyQuery(new Term("item_name", querystr), 2);
+        //Query q = new QueryParser("item_name", analyzer).parse(querystr);
+
+        int hitsPerPage = 10;
+        IndexReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopDocs docs = searcher.search(fuzzyQuery, hitsPerPage);
+        ScoreDoc[] hits = docs.scoreDocs;
+
+        log.info("Found " + hits.length + " hits.");
+
+        for(int i=0;i<hits.length;++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docs.scoreDocs[i].doc);
+            log.info((i + 1) + ". " + d.get("item_name") + "\t" + d.get("item_type"));
+        }
+
+        reader.close();
+        directory.close();
 
     }
 
